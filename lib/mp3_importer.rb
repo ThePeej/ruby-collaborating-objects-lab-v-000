dfd071ebe776fd91fb8c0a461c917535538189ad
PJ Frias <peejfrias@gmail.com>
2018-01-09 17:50:30 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -25,7 +25,7 @@ class MP3Importer
       name = file.split(" - ")[0]
       title = file.split(" - ")[1]
       song = Song.new(title)
-      song.artist = Artist.find_or_create_by_name(name)
+      song.artist = Artist.find_or_create_by_name(name).save
       # binding.pry
     end
     # binding.pry
