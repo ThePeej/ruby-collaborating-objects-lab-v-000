5b74c7656e9532ca7781b0da5607f7f8390b2caf
PJ Frias <peejfrias@gmail.com>
2018-01-09 15:35:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,8 +25,10 @@ class Artist
   def self.find_or_create_by_name(name)
     binding.pry
     if self.all.detect{|artist| artist.name == name}
+      binding.pry
       artist
     else
+      binding.pry
       Artist.new(name)
     end
   end
